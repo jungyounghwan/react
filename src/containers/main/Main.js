494ef5e3cf96f7be191616976ab5de26524636f6
jung <jung@saramin.co.kr>
2018-09-04 18:01:26 +0900

    main slide js 완료

--- src/containers/main/Main.js
+++ src/containers/main/Main.js
@@ -1,36 +1,49 @@
 import React from 'react';
 
-const Main = () => {
-        var index = 0;   //이미지에 접근하는 인덱스
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.currentDiv = this.currentDiv.bind(this);
+        this.slideShow = this.slideShow.bind(this);
+        this.state = {count: 1};
+    };
 
-        function currentDiv(n) {
-            index = n-1;
-            slideShow(index);
+    currentDiv(e) {
+        var index = e.target.dataset.count;
+        this.slideShow(index);
+    }
+
+    slideShow(n) {
+        if(n !== undefined){
+            this.state.count = n;
         }
+        var target = document.getElementsByClassName("slide");  //slide1에 대한 dom 참조
+        var dots = document.getElementsByClassName("demo");
+        for(var i = 0; i < target.length; i++) {
+            target[i].style.display = "none";   //처음에 전부 display를 none으로 한다.
+        }
+        for (var i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" on", "");
+        }
+        this.state.count++;
+        if(this.state.count > target.length) {
+            this.state.count = 1;  //인덱스가 초과되면 1로 변경
+        }
+        target[this.state.count-1].style.display = "block";  //해당 인덱스는 block으로
+        dots[this.state.count-1].className += " on";
+    };
 
-        var slideShow = setInterval(function (n) {
-            if (n !== undefined){
-                index = n;
-            }
-            var target = document.getElementsByClassName("slide");  //slide1에 대한 dom 참조
-            var dots = document.getElementsByClassName("demo");
-            console.log(index);
-            /*if (n > target.length) {index = 0}
-            if (n < 1) {index = target.length}*/
-            for(var i = 0; i < target.length; i++) {
-                target[i].style.display = "none";   //처음에 전부 display를 none으로 한다.
-            }
-            for (var i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" on", "");
-            }
-            index++;
-            if(index > target.length) {
-                index = 1;  //인덱스가 초과되면 1로 변경
-            }
-            target[index-1].style.display = "block";  //해당 인덱스는 block으로
-            dots[index-1].className += " on";
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.slideShow();
         }, 3000);
+    }
 
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
         return (
         <div id="container" className="main">
             <div className="box_slide">
@@ -40,14 +53,15 @@ const Main = () => {
                     <li className="slide"><img src="/src/resources/images/main/img_test03.jpg" alt="이미지3" /></li>
                 </ul>
                 <ul className="dot_slide">
-                    <li><button className="demo" onClick={currentDiv(this, 1)}>1</button></li>
-                    <li><button className="demo" onClick={currentDiv(this, 2)}>2</button></li>
-                    <li><button className="demo" onClick={currentDiv(this, 3)}>3</button></li>
+                    <li><button className="demo on" data-count="0" onClick={this.currentDiv}>1</button></li>
+                    <li><button className="demo" data-count="1" onClick={this.currentDiv}>2</button></li>
+                    <li><button className="demo" data-count="2" onClick={this.currentDiv}>3</button></li>
                 </ul>
             </div>
             <h2>공지</h2>
         </div>
-    )
+        );
+    }
 }
 
 export default Main;
