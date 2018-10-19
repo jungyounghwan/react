ae932aa7e4cc6cc0579050681bd99d2d351f38dc
jung <jung@saramin.co.kr>
2018-10-19 18:03:36 +0900

    tab component

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,17 +4,20 @@ import Tab from './Tab';
 class Index extends React.Component {
     render() {
         const tabList = {
-            name : ['1', '2']
+            type : 'defalt',
+            name : ['연혁', '수상내역']
         };
         const tabList2 = {
-            name : ['3', '4']
+            type : 'defalt',
+            name : ['기업비전 & 미션', '5대 핵심가치']
         };
 
 
         return (
             <div>
-                <Tab {...tabList} />
+                <Tab {...tabList} /><br />
                 <Tab {...tabList2} />
+
             </div>
 
         );
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -3,13 +3,14 @@ import React from 'react';
 class Tab extends React.Component {
 
     render(){
-        const tabProps = this.props.name;
-        const tabList = tabProps.map(
-            (name, index) => `<li className="tab_list${index}"><button type="button">${name}</button></li>`
+        const tabProps = this.props;
+        const tabPropsName = tabProps.name;
+        const tabList = tabPropsName.map(
+            (name, index) => <li key={index} className={"tab_list"+index}><button type="button">{name}</button></li>
         );
-        console.log(tabList);
+
         return (
-            <ul className={'tab'}>
+            <ul className={'tab type_' + tabProps.type}>
                 {tabList}
             </ul>
         );
