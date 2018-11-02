783a85aad1a978d7107c4981dbd09941b9a4c9a0
jung <jung@saramin.co.kr>
2018-11-02 14:27:35 +0900

    sub page layout 진행중 / tab component 완료

--- src/components/Index.js
+++ src/components/Index.js
@@ -8,16 +8,19 @@ import Lnb from '../containers/Lnb'
 class Index extends React.Component {
     render() {
         const tabList = {
+            no: 0,
             type : 'defalt',
-            name : ['연혁', '수상내역']
+            name : ['연혁', '수상내역', '3', '4']
         };
         const tabList2 = {
+            no: 1,
             type : 'defalt',
             name : ['기업비전 & 미션', '5대 핵심가치']
         };
         const tabList3 = {
+            no: 2,
             type : 's',
-            name : ['기업비전 & 미션', '5대 핵심가치']
+            name : ['smallTab1', 'smallTab22', 'smallTab333', 'smallTab444']
         };
 
 
@@ -33,9 +36,47 @@ class Index extends React.Component {
                     <div id="contents">
                         <ContentsTitle />
 
-                        <Tab {...tabList} /><br />
-                        <Tab {...tabList2} />
-                        <Tab {...tabList3} />
+                        <div className={'area_tab tabArea'+tabList.no}>
+                            <Tab {...tabList} />
+                            <div className="tab_cont">
+                                <p>1_0번째 tab</p>
+                            </div>
+                            <div className="tab_cont">
+                                <p>1_1번째 tab</p>
+                            </div>
+                            <div className="tab_cont">
+                                <p>1_2번째 tab</p>
+                            </div>
+                            <div className="tab_cont">
+                                <p>1_3번째 tab</p>
+                            </div>
+                        </div>
+
+                        <div className={'area_tab tabArea'+tabList2.no}>
+                            <Tab {...tabList2} />
+                            <div className="tab_cont">
+                                <p>2_0번째 tab</p>
+                            </div>
+                            <div className="tab_cont">
+                                <p>2_1번째 tab</p>
+                            </div>
+                        </div>
+
+                        <div className={'area_tab tabArea'+tabList3.no}>
+                            <Tab {...tabList3} />
+                            <div className="tab_cont">
+                                <p>3_0번째 tab</p>
+                            </div>
+                            <div className="tab_cont">
+                                <p>3_1번째 tab</p>
+                            </div>
+                            <div className="tab_cont">
+                                <p>3_2번째 tab</p>
+                            </div>
+                            <div className="tab_cont">
+                                <p>3_3번째 tab</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 class Tab extends React.Component {
+    targetTab = (e) => {
+        const tabArea = e.target.dataset.tabareano;
+        const tabNum = e.target.dataset.tabno;
+        this.viewTabContent(tabArea, tabNum);
+    }
+
+
+    viewTabContent = (tabArea, tabNum) => {
+        const targetArea = document.querySelector('.tabArea' + tabArea);
+        const targetTab = targetArea.querySelectorAll('.tab > li');
+        const targetCont = targetArea.querySelectorAll('.tab_cont');
+
+        if (targetCont) {
+            for (let i=0; i < targetTab.length; i++){
+                targetCont[i].style.display = 'none';
+                targetTab[i].classList.remove('active');
+            }
+            targetCont[tabNum].style.display = 'block';
+            targetTab[tabNum].classList.add('active');
+        }else {
+            return
+        }
+
+    }
 
     render(){
         const tabProps = this.props;
         const tabPropsName = tabProps.name;
         const tabList = tabPropsName.map(
-            (name, index) => <li key={index} className={"tab_list"+index}><button type="button">{name}</button></li>
+            (name, index) => <li key={index} className={`"tab_list"${index} ${index === 0 ? 'active' : ''}`}><button type="button" data-tabareano={tabProps.no} data-tabno={index} onClick={this.targetTab}>{name}</button></li>
         );
 
         return (
