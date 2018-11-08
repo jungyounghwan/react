8912b975d951b401a473c9085bc6a22de8087e20
jung <jnoah80@gmail.com>
2018-11-08 19:23:21 +0900

    페이지 작업

--- src/containers/hr/Aramin.js
+++ src/containers/hr/Aramin.js
@@ -4,8 +4,11 @@ class Aramin extends React.Component {
 
     render(){
         return (
-            <div>
-
+            <div className="hr_aramin">
+                <span><img src="/src/resources/images/hr/aramin_bg3.jpg" alt="아름다운 사람인의 나눔이 세상을 바꿉니다." /></span>
+                <p className="text_type1"><em>사람인HR은 <strong>"나눔으로 더불어 살아가는 아름다운 세상"</strong>을 만들고자</em><br />전 임직원이 참여하는 봉사단 &lt;아람인&gt;을 통해 사회공헌 활동에 앞장서고 있습니다.</p>
+                <p className="text_type2">온라인 취업포털 서비스를 시작했을 때부터 무료 서비스 제공 원칙을 고수하고 있으며,<br />2008년 사내 봉사단인 '아람인'을 발족, 보다 적극적이고 다양한 사회공헌활동을 펼치고 있습니다.</p>
+                <p className="text_type2">앞으로도 사람인HR은 기업의 사회적 책임을 다하고, 일회성의 후원, 봉사가 아니라 거창하지 않더라도<br />지속적으로 도움을 줄 수 있는 자발적인 나눔 문화를 정착시키기 위해 최선을 다하겠습니다.</p>
             </div>
         )
     }
--- src/containers/hr/VisionMission.js
+++ src/containers/hr/VisionMission.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import Tab from '../../components/Tab';
 
 class VisionMission extends React.Component {
 
     render(){
+        const tabList = {
+            no: 0,
+            type : 'defalt',
+            name : ['기업비젼 &amp; 미션', '5대 핵심가치']
+        };
         return (
-            <div>
-
+            <div className="hr_visionmission">
+                <div className={'area_tab tabArea'+tabList.no}>
+                    <Tab {...tabList} />
+                    <div className="tab_cont">
+                        <p>사람과 일을 잇는 행복 Bridge</p>
+                        <p><strong>사람중심, 소통, 신뢰, 도전, 창의 5대 핵심가치</strong>로 더 큰 꿈을 실현합니다.</p>
+                    </div>
+                    <div className="tab_cont">
+                        <h3>5대 핵심가치</h3>
+                        <p><strong>사람중심, 소통, 신뢰, 도전, 창의</strong> 5대 핵심가치로 더 큰 꿈을 실현합니다.</p>
+                    </div>
+                </div>
             </div>
         )
     }
